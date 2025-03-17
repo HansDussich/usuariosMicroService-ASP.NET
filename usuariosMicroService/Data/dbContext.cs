2d4fd99c0f1fe68c539c2f5879b388df5af5acc7
HansDussich <hansdussich2004@gmail.com>
2025-03-17 00:27:45 -0500

    Agregar archivos de proyecto.

---
+++ usuariosMicroService/Data/dbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using usuariosMicroService.Models;
+
+namespace usuariosMicroService.Data
+{
+    public class dbContext : DbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options) : base(options)
+        {
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+    
+}
